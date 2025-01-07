82b067fa4a44e982031f97d9472e9c500ff75d02
Farer <farer@hotmail.com>
2025-01-08 00:28:35 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,13 @@ public class PathFinder
     /// <param name="obstacles">A set of obstacle coordinates.</param>
     /// <returns>A tuple containing the path taken before interruption (if any),
     /// and a list of valid detour points around the encountered obstacle.</returns>
-    public (List<(int X, int Y)> Path, List<(int X, int Y)> ValidDetourPoints) FindPathUntilObstacle((int X, int Y) start, (int X, int Y) end, HashSet<(int X, int Y)> obstacles)
+    public (List<(int X, int Y)> Path, List<(int X, int Y)> ValidDetourPoints) FindPathUntilObstacle(
+            (int X, int Y) start,
+            (int X, int Y) end,
+            HashSet<(int X, int Y)> obstacles,
+            bool isReverse = false,
+            int mapSize = 15
+        )
     {
         var path = new List<(int X, int Y)>();
         int currentX = start.X;
@@ -32,7 +38,7 @@ public class PathFinder
             if (obstacles.Contains((currentX, currentY)) && (currentX, currentY) != start) // Obstacle encountered
             {
                 // Explore the edge of the encountered obstacle to find detour points.
-                var validDetourPoints = FindDetourPointsAlongEdge(start, end, (currentX, currentY), obstacles);
+                var validDetourPoints = FindDetourPointsAlongEdge(start, end, (currentX, currentY), obstacles, isReverse, mapSize);
                 return (path, validDetourPoints);
             }
 
@@ -65,7 +71,14 @@ public class PathFinder
     /// <param name="obstacleHit">The coordinates of the first encountered obstacle.</param>
     /// <param name="obstacles">A set of all obstacle coordinates.</param>
     /// <returns>A list of valid detour points.</returns>
-    private List<(int X, int Y)> FindDetourPointsAlongEdge((int X, int Y) start, (int X, int Y) end, (int X, int Y) obstacleHit, HashSet<(int X, int Y)> obstacles)
+    private List<(int X, int Y)> FindDetourPointsAlongEdge(
+            (int X, int Y) start,
+            (int X, int Y) end,
+            (int X, int Y) obstacleHit,
+            HashSet<(int X, int Y)> obstacles,
+            bool isReverse = false,
+            int mapSize = 15
+        )
     {
         var validDetourPoints = new List<(int X, int Y)>();
         var visitedEdges = new HashSet<(int X, int Y)>();
@@ -85,22 +98,91 @@ public class PathFinder
 
         var currentEdge = queue.Dequeue();
 
-        // Set exploration direction: prioritize direction towards the destination
-        var directionX = Math.Sign(end.X - currentEdge.X);
-        var directionY = Math.Sign(end.Y - currentEdge.Y);
+        var directionX = 0;
+        var directionY = 0;
+        var possibleDirections = new List<(int, int)>();
 
-        var possibleDirections = new List<(int, int)>
+        if (!isReverse)
         {
-            (directionX, 0), (0, directionY), (-directionX, 0), (0, -directionY), // Prioritized directions
-            (directionX, directionY), (directionX, -directionY), (-directionX, directionY), (-directionX, -directionY)
-        };
+            // Normal exploration: prioritize direction towards the destination
+            directionX = Math.Sign(end.X - currentEdge.X);
+            directionY = Math.Sign(end.Y - currentEdge.Y);
+        }
+        else
+        {
+            // Reverse exploration: prioritize direction towards the starting point
+            directionX = Math.Sign(start.X - currentEdge.X);
+            directionY = Math.Sign(start.Y - currentEdge.Y);
+        }
+
+        // Configure possibleDirections dynamically based on current movement
+        if (directionX != 0 && directionY != 0)
+        {
+            // Diagonal movement: prioritize diagonal, then X and Y
+            possibleDirections = new List<(int, int)>
+            {
+                (directionX, directionY),    // Diagonal towards target
+                (directionX, 0),            // Horizontal
+                (0, directionY),            // Vertical
+                (-directionX, directionY),  // Opposite diagonal
+                (directionX, -directionY),  // Opposite diagonal
+                (-directionX, 0),           // Opposite horizontal
+                (0, -directionY)            // Opposite vertical
+            };
+        }
+        else if (directionX != 0)
+        {
+            // Horizontal movement: prioritize X-axis, then diagonal
+            possibleDirections = new List<(int, int)>
+            {
+                (directionX, 0),            // Horizontal
+                (directionX, 1),            // Diagonal down
+                (directionX, -1),           // Diagonal up
+                (0, 1),                     // Vertical down
+                (0, -1),                    // Vertical up
+                (-directionX, 1),           // Opposite diagonal down
+                (-directionX, -1),          // Opposite diagonal up
+                (-directionX, 0)            // Opposite horizontal
+            };
+        }
+        else if (directionY != 0)
+        {
+            // Vertical movement: prioritize Y-axis, then diagonal
+            possibleDirections = new List<(int, int)>
+            {
+                (0, directionY),            // Vertical
+                (1, directionY),            // Diagonal right
+                (-1, directionY),           // Diagonal left
+                (1, 0),                     // Horizontal right
+                (-1, 0),                    // Horizontal left
+                (1, -directionY),           // Opposite diagonal right
+                (-1, -directionY),          // Opposite diagonal left
+                (0, -directionY)            // Opposite vertical
+            };
+        }
+        else
+        {
+            // Default fallback (stationary case)
+            possibleDirections = new List<(int, int)>
+            {
+                (1, 0), (0, 1), (-1, 0), (0, -1),
+                (1, 1), (-1, 1), (1, -1), (-1, -1)
+            };
+        }
+
+        // Filter directions to ensure they are within map bounds
+        possibleDirections = possibleDirections.Where(dir =>
+            IsWithinMapBounds(currentEdge.X + dir.Item1, currentEdge.Y + dir.Item2, mapSize, mapSize)
+        ).ToList();
+
 
         var startEdge = currentEdge;
         var currentPoint = startEdge;
 
         while (true)
         {
-            if (IsReachable(start, currentPoint, obstacles) && !validDetourPoints.Contains(currentPoint))
+            var isReachable = IsReachable(start, currentPoint, obstacles);
+            if (isReachable && !validDetourPoints.Contains(currentPoint))
             {
                 validDetourPoints.Add(currentPoint);
             }
@@ -131,6 +213,12 @@ public class PathFinder
         return validDetourPoints;
     }
 
+    private bool IsWithinMapBounds(int x, int y, int mapWidth, int mapHeight)
+    {
+        return x >= 0 && x < mapWidth && y >= 0 && y < mapHeight;
+    }
+
+
     /// <summary>
     /// Returns the adjacent tiles of a given tile.
     /// </summary>
@@ -157,28 +245,55 @@ public class PathFinder
     /// <param name="currentPath">The current path taken.</param>
     /// <param name="obstacles">The set of obstacles.</param>
     /// <returns>The optimal detour point, or null if none is found.</returns>
-    public (int X, int Y)? FindOptimalDetourPoint((int X, int Y) start, (int X, int Y) goal, (int X, int Y) selectedEdge, List<(int X, int Y)> currentPath, HashSet<(int X, int Y)> obstacles)
+    public (int X, int Y)? FindOptimalDetourPoint(
+            (int X, int Y) start,
+            (int X, int Y) goal,
+            (int X, int Y) selectedEdge,
+            List<(int X, int Y)> currentPath,
+            HashSet<(int X, int Y)> obstacles,
+            bool isReverse = false
+        )
     {
         var potentialDetours = new HashSet<(int X, int Y)>(
             GetAdjacentTiles(selectedEdge).Where(tile => !obstacles.Contains(tile))
         );
-        foreach (var point in currentPath.Where(p => !obstacles.Contains(p)))
-        {
-            potentialDetours.Add(point);
-        }
+        // foreach (var point in currentPath.Where(p => !obstacles.Contains(p)))
+        // {
+        //     potentialDetours.Add(point);
+        // }
 
         (int X, int Y)? bestDetour = null;
-        double minDistanceToGoal = double.MaxValue;
+        if(!isReverse) {
+            double minDistanceToGoal = double.MaxValue;
 
-        foreach (var detour in potentialDetours)
-        {
-            if (IsReachable(start, detour, obstacles))
+            foreach (var detour in potentialDetours)
             {
-                double distanceToGoal = CalculateDistance(detour, goal);
-                if (distanceToGoal < minDistanceToGoal)
+                var isReachable = IsReachable(start, detour, obstacles);
+                if (isReachable)
                 {
-                    minDistanceToGoal = distanceToGoal;
-                    bestDetour = detour;
+                    double distanceToGoal = CalculateDistance(detour, goal);
+                    if (distanceToGoal < minDistanceToGoal)
+                    {
+                        minDistanceToGoal = distanceToGoal;
+                        bestDetour = detour;
+                    }
+                }
+            }
+        }
+        else {
+            double maxDistanceFromStart = double.MinValue;
+
+            foreach (var detour in potentialDetours)
+            {
+                var isReachable = IsReachable(start, detour, obstacles);
+                if (isReachable)
+                {
+                    double distanceFromStart = CalculateDistance(detour, start);
+                    if (distanceFromStart > maxDistanceFromStart)
+                    {
+                        maxDistanceFromStart = distanceFromStart;
+                        bestDetour = detour;
+                    }
                 }
             }
         }
@@ -248,7 +363,12 @@ class Program
 {
     static void Main()
     {
-        var start = (X: 0, Y: 0); var goal = (X: 0, Y: 14);
+        // var start = (X: 0, Y: 0); var goal = (X: 0, Y: 14);
+        // var start = (X: 0, Y: 0); var goal = (X: 5, Y: 7);
+        // var start = (X: 5, Y: 7); var goal = (X: 0, Y: 14);
+        // var start = (X: 5, Y: 8); var goal = (X: 0, Y: 14);
+        var start = (X: 5, Y: 9); var goal = (X: 0, Y: 14);
+
         // var start = (X: 0, Y: 0); var goal = (X: 8, Y: 6);
         // var start = (X: 8, Y: 6); var goal = (X: 13, Y: 14);
         // var start = (X: 8, Y: 6); var goal = (X: 10, Y: 7);
@@ -266,17 +386,30 @@ class Program
         var pathFinder = new PathFinder();
         var mapSize = 15;
 
-        var (path, validSearchEdges) = pathFinder.FindPathUntilObstacle(start, goal, obstacles);
+        var (path, validSearchEdges) = pathFinder.FindPathUntilObstacle(start, goal, obstacles, false, mapSize);
 
-        if(validSearchEdges.Count == 0) {
+        if (validSearchEdges.Count == 0)
+        {
             DisplayMap(mapSize, start, goal, obstacles, path, null, null);
         }
-        else if(validSearchEdges.Count > 0) {
+        else if (validSearchEdges.Count > 0)
+        {
+            bool isReverse = false;
+            var (X, Y) = validSearchEdges.Last();
+            if (X <= 0 || X >= mapSize - 1)
+            {
+                isReverse = true;
+                (path, validSearchEdges) = pathFinder.FindPathUntilObstacle(start, goal, obstacles, true, mapSize);
+                if (validSearchEdges.Count == 0)
+                {
+                    DisplayMap(mapSize, start, goal, obstacles, path, null, null);
+                }
+            }
             var bestEdgePoint = validSearchEdges.Last();
             (int X, int Y)? optimalDetourPoint = null;
             if (bestEdgePoint != (-1, -1))
             {
-                optimalDetourPoint = pathFinder.FindOptimalDetourPoint(start, goal, bestEdgePoint, path, obstacles);
+                optimalDetourPoint = pathFinder.FindOptimalDetourPoint(start, goal, bestEdgePoint, path, obstacles, isReverse);
                 Console.WriteLine($"Optimal detour point: {optimalDetourPoint}");
             }
 
